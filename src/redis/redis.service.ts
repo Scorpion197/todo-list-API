007565e4997aa11245ce50a2c5813d764efdcbfe
Scorpion197 <doudou.gaw@gmail.com>
2024-05-04 12:53:31 +0100

    fix(redis): add expiration time to tokens stored in redis

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { Redis } from 'ioredis';
 import { InjectRedis } from '@nestjs-modules/ioredis';
-
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class RedisService {
-  constructor(@InjectRedis() private redis: Redis) {}
+  constructor(
+    @InjectRedis() private redis: Redis,
+    private configService: ConfigService,
+  ) {}
 
   async blackListToken(token: string): Promise<void> {
-    await this.redis.set(`token:${token}`, token);
+    const keyExpirationTime: number = parseInt(
+      this.configService.get<string>('REDIS_KEY_EXPIRATION_TIME'),
+    );
+    await this.redis.set(`token:${token}`, token, 'EX', keyExpirationTime);
   }
 
   async isTokenBlacklisted(token: string): Promise<boolean> {
