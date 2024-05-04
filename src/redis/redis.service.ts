8536f0855002a79a0bc0333fe7ab34bf262fe360
Scorpion197 <doudou.gaw@gmail.com>
2024-05-04 12:57:57 +0100

    chore(redis): rename constant name

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -10,14 +10,16 @@ export class RedisService {
   ) {}
 
   async blackListToken(token: string): Promise<void> {
-    const keyExpirationTime: number = parseInt(
-      this.configService.get<string>('REDIS_KEY_EXPIRATION_TIME'),
+    //delete token in memory after expiration time is elapsed to avoid unecessary storage in redis
+    const tokenExpirationTime: number = parseInt(
+      this.configService.get<string>('REDIS_TOKEN_EXPIRATION_TIME'),
     );
-    await this.redis.set(`token:${token}`, token, 'EX', keyExpirationTime);
+
+    await this.redis.set(`token:${token}`, token, 'EX', tokenExpirationTime);
   }
 
   async isTokenBlacklisted(token: string): Promise<boolean> {
-    const retrievedToken = await this.redis.get(`token:${token}`);
+    const retrievedToken: string = await this.redis.get(`token:${token}`);
     return retrievedToken !== null;
   }
 }
