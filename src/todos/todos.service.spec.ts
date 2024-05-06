75e67e3eb96b58a2bd5c950b9952e097e3fb074c
Scorpion197 <doudou.gaw@gmail.com>
2024-05-06 14:32:12 +0100

    fix(tests): fix find all unit test

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TodosService } from './todos.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { RedisService } from '../redis/redis.service';
+import { TodoQueryDto } from './dto/query.dto';
 
 const mockPrismaService = () => ({
   todo: {
@@ -43,6 +44,60 @@ describe('TodosService', () => {
       expect(await service.create(createTodoDto)).toEqual(createTodoDto);
       expect(prismaService.todo.create).toHaveBeenCalledWith({
         data: createTodoDto,
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return todos from cache if available', async () => {
+      const query: TodoQueryDto = { content: 'Test', sortBy: 'createdAt' };
+      const cachedData = {
+        data: [{ id: 1, content: 'Test Todo' }],
+        nextCursor: null,
+      };
+      redisService.getCachedQuery.mockResolvedValue(cachedData);
+
+      expect(await service.findAll(1, query, '5')).toEqual(cachedData);
+    });
+
+    it('should fetch todos from database and cache them if not in cache', async () => {
+      const query: TodoQueryDto = { content: 'Test', sortBy: 'createdAt' };
+      const dbData = [
+        { id: 1, content: 'Test Todo' },
+        { id: 2, content: 'Another Test Todo' },
+      ];
+      redisService.getCachedQuery.mockResolvedValue(null);
+      prismaService.todo.findMany.mockResolvedValue(dbData);
+      redisService.cacheQuery.mockResolvedValue(undefined); // Simulate caching
+
+      const expectedResponse = { data: dbData, nextCursor: null };
+      expect(await service.findAll(1, query, '2')).toEqual(expectedResponse);
+      expect(redisService.cacheQuery).toHaveBeenCalledWith(
+        query,
+        expectedResponse,
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update a todo', async () => {
+      const updateTodoDto = { content: 'Updated Content' };
+      const updatedTodo = { id: 1, userId: 1, content: 'Updated Content' };
+      prismaService.todo.update.mockResolvedValue(updatedTodo);
+      expect(await service.update(1, 1, updateTodoDto)).toEqual(updatedTodo);
+      expect(prismaService.todo.update).toHaveBeenCalledWith({
+        where: { id: 1, userId: 1 },
+        data: updateTodoDto,
+      });
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a todo', async () => {
+      prismaService.todo.delete.mockResolvedValue({ id: 1, userId: 1 });
+      await service.delete(1, 1);
+      expect(prismaService.todo.delete).toHaveBeenCalledWith({
+        where: { id: 1, userId: 1 },
       });
     });
   });
